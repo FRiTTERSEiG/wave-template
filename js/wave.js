ed2d7d0ef566d37dadbda8becd01b5e20ee1d99f
Sarah Hui <sarah.a.hui@gmail.com>
2019-10-03 10:39:26 +1300

    Add a 3rd orange wave

--- js/wave.js
+++ js/wave.js
@@ -3,15 +3,22 @@
 var width, height, center;
 var points = 10;
 var smooth = true;
-var path = new Path();
-var path2 = new Path();
 var mousePos = view.center / 2;
 var pathHeight = mousePos.y;
+
+var path = new Path();
+var path2 = new Path();
+var path3 = new Path();
+
 path.fillColor = 'black';
 path2.fillColor = 'pink';
+path3.fillColor = 'orange';
+
 initializePath(path);
 initializePath(path2);
+initializePath(path3);
 
+// Pass path variable to intialise
 function initializePath(p) {
 	center = view.center;
 	width = view.size.width;
@@ -26,6 +33,7 @@ function initializePath(p) {
 	p.fullySelected = true;
 }
 
+// Called by Paper.js on each frame
 function onFrame(event) {
 	pathHeight += (center.y - mousePos.y - pathHeight) / 10;
 
@@ -45,15 +53,25 @@ function onFrame(event) {
 		path2.segments[i].point.y = yPos;
 	}
 
+	// Path 3
+	for (var i = 1; i < points; i++) {
+		var sinSeed = event.count + (i + i % 5) * 50;
+		var sinHeight = Math.sin(sinSeed / 100) * pathHeight;
+		var yPos = Math.sin(sinSeed / 50) * sinHeight + height;
+		path3.segments[i].point.y = yPos;
+	}
+
 	if (smooth)
 		path.smooth({ type: 'continuous' });
 		path2.smooth({ type: 'continuous' });
+		path3.smooth({ type: 'continuous' });
 }
 
 function onMouseMove(event) {
 	mousePos = event.point;
 }
 
+// Called by Paper.js on mouse move
 function onMouseDown(event) {
 	// smooth = !smooth;
 	// if (!smooth) {
@@ -66,8 +84,10 @@ function onMouseDown(event) {
 	// }
 }
 
+// Called by Paper.js on resize
 // Reposition the path whenever the window is resized:
 function onResize(event) {
 	initializePath(path);
 	initializePath(path2);
+	initializePath(path3);
 }
