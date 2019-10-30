8e8bfcabd80bb4bacff8da3b300cbd1d86a83314
FRiTTERSiEG <clars17@mywhitecliffe.com>
2019-10-31 11:03:35 +1300

    titles

--- js/wave.js
+++ js/wave.js
@@ -4,7 +4,7 @@
 
 var width, height, center;
 var points = 10;
-var smooth = false;
+var smooth = true;
 var mousePos = view.center / 2;
 var pathHeight = mousePos.y;
 
@@ -26,12 +26,12 @@ function initializePath(p) {
 	width = view.size.width;
 	height = view.size.height / 2;
 	p.segments = [];
-	p.add(view.bounds.bottomLeft);
-	for (var i = 1; i < points; i++) {
-		var point = new Point(width / points * i, center.y);
-		p.add(point);
-	}
-	p.add(view.bounds.bottomRight);
+	//p.add(view.bounds.bottomLeft);
+	//for (var i = 1; i < points; i++) {
+	//	var point = new Point(width / points * i, center.y);
+	//	p.add(point);
+	//}
+	//p.add(view.bounds.bottomRight);
 	p.fullySelected = true;
 }
 
