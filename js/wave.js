51e4b5fc6635d67b102bb3d5eed6734f84e543ef
FRiTTERSiEG <clars17@mywhitecliffe.com>
2019-10-31 14:18:36 +1300

    titles

--- js/wave.js
+++ js/wave.js
@@ -94,3 +94,18 @@ function onResize(event) {
 	initializePath(path2);
 	initializePath(path3);
 }
+
+function getRandomColor() {
+var letters = '0123456789ABCDEF';
+var color = '#';
+for (var i = 0; i < 6; i++) {
+	color += letters[Math.floor(Math.random() * 16)];
+}
+return color;
+}
+
+$("#asset1").click(function(){
+var path.fillColor = getRandomColor();
+path2.fillColor = getRandomColor();
+path3.fillColor = getRandomColor();
+});
